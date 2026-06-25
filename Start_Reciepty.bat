d685ebca3de19982b32afc77902625bad98fa6e2
sergej <sergej.cupkovic2@gmail.com>
2026-06-25 15:46:24 +0200

    hotfix

--- Start_Reciepty.bat
+++ Start_Reciepty.bat
@@ -1,5 +1,14 @@
 @echo off
 echo Starting Reciepty...
 cd /d "%~dp0"
-call .venv\Scripts\activate.bat
+IF NOT EXIST .venv (
+    echo Creating virtual environment...
+    python -m venv .venv
+    call .venv\Scripts\activate.bat
+    echo Installing requirements...
+    pip install -r requirements.txt
+) ELSE (
+    call .venv\Scripts\activate.bat
+)
 python run.py
+pause
